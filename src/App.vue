6fe26f474823ebfed4f386759b068489496fddce
IvanaSraka <ivana.sraka@am2studio.hr>
2025-10-11 15:53:56 +0200

    Opis promjena

--- src/App.vue
+++ src/App.vue
@@ -24,9 +24,7 @@ const imagePaths = [
   new URL('./assets/image/bg-15.jpg', import.meta.url).href,
   new URL('./assets/image/bg-16.jpg', import.meta.url).href,
   new URL('./assets/image/bg-17.jpg', import.meta.url).href,
-  new URL('./assets/image/karta.png', import.meta.url).href,
-  new URL('./assets/image/patuljak-zabrinut.png', import.meta.url).href,
-  new URL('./assets/image/patuljak-zabrinut-2.png', import.meta.url).href,
+  new URL('./assets/image/karta.png', import.meta.url).href, 
 ];
  
 onMounted(() => {
@@ -54,9 +52,7 @@ const bg11 = `url(${new URL('./assets/image/bg-11.jpg', import.meta.url).href})`
 const bg15 = `url(${new URL('./assets/image/bg-15.jpg', import.meta.url).href})`;
 const bg16 = `url(${new URL('./assets/image/bg-16.jpg', import.meta.url).href})`;
 const bg17 = `url(${new URL('./assets/image/bg-17.jpg', import.meta.url).href})`;
-const bgkarta = `url(${new URL('./assets/image/karta.png', import.meta.url).href})`;
-const bgzabrinut = `url(${new URL('./assets/image/patuljak-zabrinut.png', import.meta.url).href})`;
-const bgzabrinut2 = `url(${new URL('./assets/image/patuljak-zabrinut-2.png', import.meta.url).href})`;
+const bgkarta = `url(${new URL('./assets/image/karta.png', import.meta.url).href})`; 
 
 const no1 = ref('');
 const no2 = ref('');
@@ -71,10 +67,12 @@ const no10 = ref('');
 
 const handleInput = (event, model) => {
   const value = event.target.value;
-  if (/^\d*$/.test(value)) {
-    if (model === 'no1') no1.value = value;
+  if (model === 'no1') {
+    no1.value = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+  } else if (model === 'no3') {
+    no3.value = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+  } else if (/^\d*$/.test(value)) {
     if (model === 'no2') no2.value = value;
-    if (model === 'no3') no3.value = value;
     if (model === 'no4') no4.value = value;
     if (model === 'no5') no5.value = value;
     if (model === 'no6') no6.value = value;
@@ -136,7 +134,7 @@ const { nextSlide, backSlide } = arrowSlides();
         </div>    
 
         <div class="scene scene-5 ff-v2 font-size absolute hidden" :style="{ backgroundImage: bg4, backgroundSize: 'cover', backgroundPosition: 'center' }" style="z-index: 46">
-            <div class="wrapper-v5 absolute botton-text"><span class="text-ovelay">Naime, naš patuljak je hodao dalje nego što je mislio i predaleko zalutao. 
+            <div class="wrapper-v2 botton-text"><span class="text-ovelay">Naime, naš patuljak je hodao dalje nego što je mislio i predaleko zalutao. 
             Još je i samo engleski pričao, a u selu gnomova engleski nitko nije znao. Kako će se moći kući vratiti, ako ne može s njima ni razgovarati? A već i trbuščić svoju hranu traži, ali riječ Food, gnomove još više razdraži.</span>
             </div>  
             <button class="btn next-btn absolute" @click="nextSlide">
@@ -147,8 +145,8 @@ const { nextSlide, backSlide } = arrowSlides();
             </button>
         </div>     
 
-        <div class="scene scene-6 ff-v2 font-size absolute hidden" :style="{ backgroundImage: bg6, backgroundSize: 'cover', backgroundPosition: 'left' }" style="z-index: 45">
-            <div class="wrapper-v4 absolute botton-text"><span class="text-ovelay">Gnomovi su mislili da ih patuljak zeza, jer do sad nisu vidjeli tako velikog uljeza. Iako su znali da negdje žive divovi patuljci, nikada ih nisu susreli ili s njima razgovarali. Sada naš patuljak tvoju pomoć treba, jer ga svaki gnom već ljutito gleda.</span>
+        <div class="scene scene-6 ff-v2 font-size absolute hidden" :style="{ backgroundImage: bg6, backgroundSize: 'cover', backgroundPosition: 'center' }" style="z-index: 45">
+            <div class="wrapper-v2 relative justify-end"><span class="text-ovelay">Gnomovi su mislili da ih patuljak zeza, jer do sad nisu vidjeli tako velikog uljeza. Iako su znali da negdje žive divovi patuljci, nikada ih nisu susreli ili s njima razgovarali. Sada naš patuljak tvoju pomoć treba, jer ga svaki gnom već ljutito gleda.</span>
             </div>  
             <button class="btn next-btn absolute" @click="nextSlide">
                 <img src="/src/assets/image/arrow.svg" />
@@ -159,9 +157,9 @@ const { nextSlide, backSlide } = arrowSlides();
         </div> 
 
         <div class="scene scene-7 ff-v2 font-size absolute hidden" :style="{ backgroundImage: bg6a, backgroundSize: 'cover', backgroundPosition: 'center' }" style="z-index: 44">
-            <div class="wrapper-v5 absolute botton-text"><span class="text-ovelay">Pomozi našem patuljku kada treba prevoditi, i tako mu pomogni da se vrati kući. 
-                Ali patuljak želi znati tvoje ime, da te može pozvati u pomoć!</span>
-            <input type="text" name="no-1" v-model="no1" placeholder="Your name" class="input-line font-size font-size--v1" @input="handleInput($event, 'no1')" />
+            <div class="wrapper-v1 relative"><span class="text-ovelay">Pomozi našem patuljku kada treba prevoditi, i tako mu pomogni da se vrati kući. 
+                Ali patuljak želi znati tvoje ime, da te može pozvati u pomoć!<br /></span>
+            <input type="text" name="no-1" v-model="no1" placeholder="Your name" class="input-line font-size" @input="handleInput($event, 'no1')" />
             </div>  
             <button class="btn next-btn absolute" v-if="no1 !== ''" @click="nextSlide">
                 <img src="/src/assets/image/arrow.svg" />
@@ -172,10 +170,10 @@ const { nextSlide, backSlide } = arrowSlides();
         </div> 
 
         <div class="scene scene-8 ff-v2 font-size absolute hidden" :style="{ backgroundImage: bg7, backgroundSize: 'cover', backgroundPosition: 'center' }" style="z-index: 43">
-            <div class="wrapper-v1 relative justify-end"><span class="text-ovelay">Hvala ti, sada ćeš mu moći pomoći kuči se vratiti. Iako patuljak želi kući vratiti, jako je gladan, trbuščić mu već kruli. <br > 
-                Možeš li mu pomoći {{ no1 }} i reći što znači Food u gnomovom govoru? </span>
+            <div class="wrapper-v1 relative justify-end"><span class="text-ovelay">Hvala ti, sada ćeš mu moći pomoći! Iako se patuljak želi kući vratiti, jako je gladan, trbuščić mu već kruli. 
+                Možeš li mu pomoći {{ no1 }} i reći što znači Food u gnomovom govoru? <br > </span>
             <span>
-                <input type="text" name="no-2" v-model="no2" class="input-line font-size font-size--v1" @input="handleInput($event, 'no2')" />
+                <input type="text" name="no-2" v-model="no2" placeholder="Prevedi Food" class="input-line font-size font-size--v1" @input="handleInput($event, 'no2')" />
                 <span class="validation-width"> 
                     <span v-if="no2 && no2 !== ''" :class="['validation', no2 === 'hrana' ? 'correct' : 'incorrect']">
                         {{  no2?.toLowerCase() === 'hrana' ? '✔️' : '❌' }}
@@ -192,7 +190,7 @@ const { nextSlide, backSlide } = arrowSlides();
         </div>
 
         <div class="scene scene-9 ff-v2 font-size absolute hidden" :style="{ backgroundImage: bg8, backgroundSize: 'cover', backgroundPosition: 'center' }" style="z-index: 42">
-            <div class="wrapper-v1 relative"><span class="text-ovelay">Čim su gnomovi shvatili da je naš patuljak gladuš pravi, odmah su se razveselili i pun stol hrane poslužili. Također su željeli znati koji mu je obrok sa stola najdraži.Je li to bila patka ili jabuka, možda komad pileta ili zrno grožđa? Patuljak opet traži pomoć tvoju, pa mu prevedi riječ pokoju. Koje ti je najdraže jelo?  </span>
+            <div class="wrapper-v1 relative"><span class="text-ovelay">Čim su gnomovi shvatili da je naš patuljak gladuš pravi, odmah su se razveselili i pun stol hrane poslužili. Također su željeli znati koji mu je obrok sa stola najdraži.<br />Je li to bila patka ili jabuka, možda komad pileta ili zrno grožđa? Patuljak opet traži pomoć tvoju, pa mu prevedi riječ pokoju. Koje ti je najdraže jelo?  <br/></span>
                 <input type="text" name="no-3" v-model="no3" class="input-line font-size font-size--v1" @input="handleInput($event, 'no3')" />
             </div> 
             <button class="btn next-btn absolute" v-if="no3 !== ''"  @click="nextSlide">
@@ -204,8 +202,9 @@ const { nextSlide, backSlide } = arrowSlides();
         </div>
 
         <div class="scene scene-10 ff-v2 font-size absolute hidden" :style="{ backgroundImage: bg9, backgroundSize: 'cover', backgroundPosition: 'center' }" style="z-index: 41">
-            <div class="wrapper-v1 relative"><span class="text-ovelay">{{no3}} je i gnomovima hrana najdraža. A još ih zanima, kako se kaže:</span>
-                <span>
+            <div class="wrapper-v1 relative">
+                <span class="text-ovelay">{{no3}} je i gnomovima hrana najdraža. Još ih zanima, kako se na engleskom kaže:<br /></span>
+                <span class="text-bg">
                     <input type="text" name="no-4" placeholder="Jabuka" v-model="no4" class="input-line font-size font-size--v1" @input="handleInput($event, 'no4')" />
                     <span class="validation-width"> 
                         <span v-if="no4 && no4 !== ''" :class="['validation', no4 === 'apple' ? 'correct' : 'incorrect']">
@@ -214,7 +213,7 @@ const { nextSlide, backSlide } = arrowSlides();
                     </span>
                 </span>
 
-                <span>
+                <span class="text-bg">
                     <input type="text" name="no-5" placeholder="Patka" v-model="no5" class="input-line font-size font-size--v1" @input="handleInput($event, 'no5')" />
                     <span class="validation-width"> 
                         <span v-if="no5 && no5 !== ''" :class="['validation', no5 === 'duck' ? 'correct' : 'incorrect']">
@@ -223,7 +222,7 @@ const { nextSlide, backSlide } = arrowSlides();
                     </span>
                 </span>
 
-                <span>
+                <span class="text-bg">
                     <input type="text" name="no-6" placeholder="Jaja" v-model="no6" class="input-line font-size font-size--v1" @input="handleInput($event, 'no6')" />
                     <span class="validation-width"> 
                         <span v-if="no6 && no6 !== ''" :class="['validation', no6 === 'eggs' ? 'correct' : 'incorrect']">
@@ -232,7 +231,7 @@ const { nextSlide, backSlide } = arrowSlides();
                     </span>
                 </span>
 
-                <span>
+                <span class="text-bg">
                     <input type="text" name="no-7" placeholder="Narandža" v-model="no7" class="input-line font-size font-size--v1" @input="handleInput($event, 'no7')" />
                     <span class="validation-width"> 
                         <span v-if="no7 && no7 !== ''" :class="['validation', no7 === 'orange' ? 'correct' : 'incorrect']">
@@ -250,8 +249,8 @@ const { nextSlide, backSlide } = arrowSlides();
         </div>
 
         <div class="scene scene-11 ff-v2 font-size absolute hidden" :style="{ backgroundImage: bg10, backgroundSize: 'cover', backgroundPosition: 'center' }" style="z-index: 40">
-            <div class="wrapper-v1 relative float-right"><span class="text-ovelay">Zahvaljujući tvojoj pomoći, patuljak više nema problema i nakon što je napunio svoj trbuščić, želi naći mjesto da malo zadrijema. Ispod drveta bolje ne, da mu jabuka na glavu ne padne, pa možda da zamoli gnomove za najbolje mjesto za oči umorne. Ponovno treba pomoć tvoju za riječ Sleeping na patuljčevu govoru.</span>
-                <input type="text" name="no-8" v-model="no8" class="input-line font-size font-size--v1" @input="handleInput($event, 'no8')" />
+            <div class="wrapper-v1 relative float-right"><span class="text-ovelay">Zahvaljujući tvojoj pomoći, patuljak više nema problema i nakon što je napunio svoj trbuščić, želi naći mjesto da malo zadrijema. Ispod drveta bolje ne, da mu jabuka na glavu ne padne, pa možda da zamoli gnomove za najbolje mjesto za oči umorne. Ponovno treba pomoć tvoju za riječ Sleeping na patuljčevu govoru.<br /></span>
+                <input type="text" name="no-8" v-model="no8" class="input-line font-size" placeholder="Sleeping na hrvatskom?" @input="handleInput($event, 'no8')" />
             </div> 
             <button class="btn next-btn absolute" v-if="no8?.toLowerCase() === 'spavanje'"  @click="nextSlide">
                 <img src="/src/assets/image/arrow.svg" />
@@ -284,10 +283,12 @@ const { nextSlide, backSlide } = arrowSlides();
             </button>        
         </div>
 
-        <div class="scene scene-14 ff-v2 font-size absolute hidden" :style="{ backgroundImage: bg11, backgroundSize: 'cover', backgroundPosition: 'center' }" style="z-index: 37">
-            <div class="wrapper-v1 relative"><span class="text-ovelay">Patuljak naš gnomu je pomoć pružio i brzo rupu na krovu pokrpao. Gnomovi su bili jako sretni i željeli su da ih naš patuljak ponovno posjeti. Kako bi znao opet navratiti, napravili su mu kartu pa će se sada moći i kući vratiti. Patuljak im se želio zahvaliti, pa riječ thank you prevedi.</span>
+        <div class="scene scene-14 ff-v2 font-size absolute hidden" :style="{ backgroundImage: bg16, backgroundSize: 'cover', backgroundPosition: 'center' }" style="z-index: 37">
+            <div class="wrapper-v1 absolute"><span class="text-ovelay">Patuljak naš gnomu je pomoć pružio i brzo rupu na krovu pokrpao. Gnomovi su bili jako sretni i željeli su da ih naš patuljak ponovno posjeti. Kako bi znao opet navratiti, napravili su mu kartu pa će se sada moći i kući vratiti. Patuljak im se želio zahvaliti, pa riječ thank you prevedi.</span>
                 <input type="text" name="no-10" v-model="no10" class="input-line font-size font-size--v1" @input="handleInput($event, 'no10')" />
-            </div> 
+            </div>        
+            <div class="wrapper-v1 relative karta flex">  <img src="/src/assets/image/karta.png" class="mx-auto z-index-auto" /> </div>     
+            
             <button class="btn next-btn absolute" v-if="no10?.toLowerCase() === 'hvala'" @click="nextSlide">
                 <img src="/src/assets/image/arrow.svg" />
             </button>
@@ -296,14 +297,15 @@ const { nextSlide, backSlide } = arrowSlides();
             </button>        
         </div>
 
-        <div class="scene scene-15 ff-v2 font-size absolute hidden" :style="{ backgroundImage: bg11, backgroundSize: 'cover', backgroundPosition: 'center' }" style="z-index: 36">
-            <div class="wrapper-v1 relative"><span class="text-ovelay">Sad već i gnomovi malo engleski jezik znaju, pa će ga početi govoriti i u svome kraju. Patuljak je već put kući krenuo, sretan što je stekao nove prijatelje. Shvatio je da pravi prijatelji ne moraju govoriti isti jezik, važno je samo da se razumiju srcem.</span>
-                <input type="text" name="no-11" v-model="no11" class="input-line font-size font-size--v1" @input="handleInput($event, 'no11')" />
+        <div class="scene scene-15 ff-v2 font-size absolute hidden" :style="{ backgroundImage: bg17, backgroundSize: 'cover', backgroundPosition: 'center' }" style="z-index: 36">
+            <div class="wrapper-v1 relative"><span class="text-ovelay">Sad već i gnomovi malo engleski jezik znaju, pa će ga početi govoriti i u svome kraju. Patuljak je već put kući krenuo i jedva čekao priliku za iduću posjetu.
+            </span> 
             </div> 
-            <a href="https://price-pitalice.com.hr/" class="btn btn-submit next-btn absolute">Kraj</a>
+            
+            <a href="https://price-pitalice.com.hr/" class="btn btn-submit next-btn absolute right-20">Kraj</a>
             <button class="btn back-btn absolute" @click="backSlide">
                 <img src="/src/assets/image/arrow-prev.svg" />
-            </button>        
+            </button>       
         </div>
 
     </div> 
